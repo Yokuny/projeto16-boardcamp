45481b6e9596f10b0c13de9d943f326810a69727
yokuny <felipe.vni@hotmail.com>
2023-05-11 09:54:30 -0300

    db connection

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,1 +1,12 @@
+import pg from "pg";
+import { config } from "dotenv";
+config();
 
+const dbName = "boardcamp";
+
+const connection = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  database: dbName,
+});
+
+export default connection;
