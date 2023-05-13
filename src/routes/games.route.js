7c8bc67f02717bec3c87af79edabbbac858491ba
yokuny <felipe.vni@hotmail.com>
2023-05-13 19:40:53 -0300

    removing img url validate

--- src/routes/games.route.js
+++ src/routes/games.route.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 import route from "../controllers/games.controller.js";
 import gamesSchema from "../schemas/games.schema.js";
 import validate from "../middlewares/data.middleware.js";
-import imageValidate from "../middlewares/image.middleware.js";
+// import imageValidate from "../middlewares/image.middleware.js";
 
 const games = Router();
 
 games.get("/games", route.getGames);
-games.post("/games", validate(gamesSchema), imageValidate(), route.postGame);
+games.post("/games", validate(gamesSchema), route.postGame);
 
 export default games;
